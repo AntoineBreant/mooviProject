70846e3046ea69fd8f505e56488babe9117e7258
antoine <antoine.breant29@gmail.com>
2020-01-09 13:57:25 +0100

    pret pour pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlbumsComponent } from './components/albums/albums.component';
-import { PhotosComponent } from './components/photos/photos.component';
 import { ListeFilmComponent } from './components/liste-film/liste-film.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import { FilmComponent } from './components/film/film.component';
 import { CommentaireComponent } from './components/commentaire/commentaire.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlbumsComponent,
-    PhotosComponent,
     ListeFilmComponent,
     FilmComponent,
-    CommentaireComponent
+    CommentaireComponent,
+    LoginUserComponent
   ],
   imports: [
     BrowserModule,
