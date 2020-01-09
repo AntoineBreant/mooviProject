7df68adb011b2358501a9b2b98626fa7c27a3296
antoine <antoine.breant29@gmail.com>
2020-01-09 13:42:35 +0100

    pret pour pull general

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AlbumsComponent } from './components/albums/albums.component';
-import { PhotosComponent } from './components/photos/photos.component';
 import {ListeFilmComponent} from './components/liste-film/liste-film.component';
 import {FilmComponent} from './components/film/film.component';
 
