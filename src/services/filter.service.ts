f1ed74fb92e56a8deb2df2424ab9f9ca6fc68514
antoine <antoine.breant29@gmail.com>
2020-02-14 15:54:24 +0100

    ajout systeme de filtre

--- src/services/filter.service.ts
+++ src/services/filter.service.ts
@@ -21,9 +21,32 @@ export class FilterService {
      return this.genre;
    }
 
-   shouldDisplay(idGenre){
+   shouldDisplay(idGenre,annee){
+
+    return this.testGenre(idGenre)&&this.testAnnee(annee);
+
+   }
+
+   testAnnee(date){
+      if(this.maxYear==null)
+        return true;
+
+      if(date==null||date=='undefined')
+        return false;
+      let annee:Date= new Date(date);
+
+      if(annee.getFullYear()<=this.maxYear&&annee.getFullYear()>=this.minYear){
+        return true;
+      }
+   }
+
+
+   testGenre(idGenre){
     if(this.genre==null)
       return true;
+
+    if(idGenre==null||idGenre=='undefined')
+      return false;
 
     for(let g of idGenre){
       if (g.con_idGenre==this.genre)
@@ -31,8 +54,8 @@ export class FilterService {
     }
  
     return false;
-
    }
+
 
    setYearRange(min,max){
     this.minYear=min;
